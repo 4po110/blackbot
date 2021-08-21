a89df6097c622d6843918ce01d67e5cf91a2c0b2
amblade <qcryptobase@gmail.com>
2021-08-22 02:45:12 +1000

    some change

--- utils/verify.py
+++ utils/verify.py
@@ -43,7 +43,7 @@ def verify(driver, email, password):
         parent_handle = driver.window_handles[0]
         time.sleep(1)
         driver.find_element_by_xpath('//a[normalize-space()="Click here to verify account"]').click()
-        time.sleep(10)
+        time.sleep(15)
         child_handle = [x for x in driver.window_handles if x != parent_handle][0]
         driver.switch_to.window(child_handle)
         time.sleep(5)
