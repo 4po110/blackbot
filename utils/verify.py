680f4bec06ad7d08e1175d4873cf96fd2c2ea0c9
amblade <qcryptobase@gmail.com>
2021-08-22 07:02:48 +1000

    some change

--- utils/verify.py
+++ utils/verify.py
@@ -48,7 +48,7 @@ def verify(driver, email, password):
             pass
         time.sleep(10)
         print('removed popup')
-        driver.find_element_by_xpath('//span[contains(text(),"Verify your CoinMarketCap account")]').click()
+        driver.find_element_by_xpath('//div[contains(@role,"listbox")]/div/div/div[1]/div/div').click()
         time.sleep(5)
         parent_handle = driver.window_handles[0]
         time.sleep(1)
